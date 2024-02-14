6f0fcd3fb08dffc7e144007c64f61d2c0e9637c3
Evil Potato <supronva@gmail.com>
2024-02-14 13:00:32 +0300

    add seeds for question and answer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Data is loading'
+
+questions = Question.create([
+                              { title: 'Как в VS Code перейти в класс или переменную?', body: 'В phpStorm или rubyMine например зажимаешь ctrl и жмёшь на класс или переменную автоматически переходит к ней, а как в vs code такое сделать?' }
+                            ])
+
+answers = Answer.create([
+                          { body: 'Клавиша alt ...', question: questions[0] },
+                          { body: 'Зажимаешь alt и жмёшь на класс или переменную', question: questions[0] }
+                        ])
+puts 'Data loaded!'
