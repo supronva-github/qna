ceb8e8a2017fdf103fa087b113f5e3abc329a4b5
Evil Potato <supronva@gmail.com>
2024-02-29 02:57:33 +0300

    Add rspec test for controller questions action:create

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 6.1.0'
   gem 'factory_bot_rails'
+  gem 'pry'
 end
 
 group :development do
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe QuestionsController, type: :controller do
   let(:question) { create(:question) }
@@ -50,6 +51,32 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'renders edit view' do
       expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'save a new question in the database' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params:  { question: attributes_for(:question) }
+
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+
+        expect(response).to render_template :new
+      end
     end
   end
 end
