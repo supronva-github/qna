c495f401509a65ae2e48c171d4da8dab46bc84a9
Vitaliy Supron <supronva@gmail.com>
2024-11-07 23:22:30 +0300

    add rspec for answer controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,15 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+  let(:answer) { create(:answer, question: question) }
 
   describe 'GET #new' do
-    let(:question) { create (:question) }
-
     before { get :new, params: { question_id: question } }
 
-    it "" do
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
 
+  describe 'GET #show' do
+    it 'renders show view' do
+      get :show, params: { id: answer }
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe 'POST #create' do
+    let!(:answers) { create_list(:answer, 3, question: question) }
+    let(:valid_params) { attributes_for(:answer) }
+    let(:invalid_params) { attributes_for(:answer, :invalid) }
+
+    subject { post :create, params: { question_id: question, answer: answer_params } }
+
+    context 'with valid attributes' do
+      let(:answer_params) { valid_params }
+
+      it 'saves a new answer in database' do
+        expect { subject }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        subject
+        expect(response).to redirect_to assigns(:answer)
+      end
     end
 
+    context 'with invalid attributes' do
+      let(:answer_params) { invalid_params }
+
+      it 'does not save the answer' do
+        expect { subject }.to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        subject
+        expect(response).to render_template :new
+      end
+    end
   end
 end
