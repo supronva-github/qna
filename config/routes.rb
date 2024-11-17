2e7f0f35d4b225406c61dd031eb8e3ad9e3edd09
Vitaliy Supron <supronva@gmail.com>
2024-11-17 18:05:19 +0300

    add acceptance test for sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "questions#index"
+
+  devise_for :users, path_names: { sign_in: :login, sign_out: :logout }
 
   resources  :questions do
     resources :answers, shallow: true, except: %i[index edit destroy]
--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -5,4 +5,11 @@ module FeatureHelpers
     fill_in 'Password', with: user.password
     click_on 'Log in'
   end
+
+  def sign_up(email:, password:, password_confirmation:)
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password_confirmation
+    click_button 'Sign up'
+  end
 end
