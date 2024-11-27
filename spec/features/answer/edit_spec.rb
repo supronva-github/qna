8b34e4b29efbedcaf7c4ad8be1549db37bf87d6e
Vitaliy Supron <supronva@gmail.com>
2024-11-27 22:27:27 +0300

    refactoring feature test for  update answers

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -8,6 +8,7 @@ feature 'User can edit his answer', %q{
 
   given!(:user) { create(:user) }
   given!(:question) { create(:question, :with_answers, author: user) }
+  given(:other_user) { create(:user) }
 
   scenario 'Unauthenticated can not edit answer' do
     visit question_path(question)
@@ -16,26 +17,42 @@ feature 'User can edit his answer', %q{
   end
 
   describe 'Authenticated user', js: true do
-    scenario 'edits his answer' do
-      sign_in(user)
-      visit question_path(question)
-      click_on 'Edit'
+    context 'edits his answer' do
+      background do
+        sign_in(user)
+        visit question_path(question)
+        click_on 'Edit'
+      end
 
-      within '.answers' do
-        fill_in 'You answer', with: 'edited answer'
+      scenario 'successfully' do
+        within '.answers' do
+          fill_in 'You answer', with: 'edited answer'
 
-        click_on 'Save'
+          click_on 'Save'
 
-        expect(page).to_not have_content question.answers.first.body
-        expect(page).to have_content 'edited answer'
-        expect(page).to_not have_selector 'textarea'
+          expect(page).to_not have_content question.answers.first.body
+          expect(page).to have_content 'edited answer'
+          expect(page).to_not have_selector 'textarea'
+        end
+      end
+
+      scenario 'not successful' do
+        within '.answers' do
+          fill_in 'You answer', with: ''
+          click_on 'Save'
+        end
+
+        within '.answers-erros' do
+          expect(page).to have_content "Body can't be blank"
+        end
       end
     end
 
-    scenario 'edits his answer whith erros' do
-    end
-
     scenario "tries to edit other user's answer" do
+      sign_in(other_user)
+      visit question_path(question)
+
+      expect(page).to have_no_link 'Edit'
     end
   end
 end
