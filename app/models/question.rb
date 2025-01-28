eabdaff75a068a541cebaf168c95dafe2e8e8fe8
Vitaliy Supron <supronva@gmail.com>
2025-01-28 15:44:30 +0300

    add active storage

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,5 +3,7 @@ class Question < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :best_answer, class_name: 'Answer', optional: true
 
+  has_one_attached :file
+
   validates :title, :body, presence: true
 end
