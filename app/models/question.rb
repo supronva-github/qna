35c5389543e35aea54feeeff32726bf808a417a9
Vitaliy Supron <supronva@gmail.com>
2025-01-28 18:47:46 +0300

    add multiple files to AS

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :best_answer, class_name: 'Answer', optional: true
 
-  has_one_attached :file
+  has_many_attached :files
 
   validates :title, :body, presence: true
 end
